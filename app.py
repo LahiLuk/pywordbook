53d89efda2de3252bd21bcd25b07239711400dfe
Lahorka <lahorka.nikolovski@gmail.com>
2022-01-29 21:32:39 +0100

    feat: Add login page with bootstrap css

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
 words = ["Pura", "Na", "Krovu", "Ikce", "Pikce"]
 
 @app.route('/')
-def main():
+def home():
     message = 'iso medo u ducan'
     return render_template('index.html', message=message, words=words)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return ""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+
+    return render_template('login.html', error=error)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
